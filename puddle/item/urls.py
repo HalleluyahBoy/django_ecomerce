a3ca698e6cad7cb5b5f3beee5bf0b06f3d908034
HalleluyahBoy <eejimone@gmail.com>
2023-02-24 08:02:23 +0100

    My first E-Comerce app

---
+++ puddle/item/urls.py
@@ -0,0 +1,19 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+
+from core.views import index, contact
+
+
+from django.urls import path
+
+
+from . import views
+
+app_name = 'item'
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('contact/', contact, name='contact'),
+    path('<int:pk>/', views.detail, name='detail'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
